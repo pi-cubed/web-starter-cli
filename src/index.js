2b57092a561a2a13bae17cd50292471a5187282e
Dylan Richardson <dylanrichardson1996@gmail.com>
2018-02-26 17:32:28 -0500

    feat: Customize app name and description.
    close #10 close #11

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 const inquirer = require('inquirer');
 const download = require('download-git-repo');
+const replace = require('replace-in-file');
+
+const NAME = 'web-starter';
+const DESCRIPTION = 'Starter kit for making web apps using JS';
 
 exports = module.exports = () => {
   inquirer
     .prompt([
       {
         type: 'input',
-        name: 'app',
+        name: 'name',
         message: "What's the name of your app?"
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: "What's the description of your app?"
       }
     ])
-    .then(({ app }) => {
-      download('drich14/web-starter', app, err =>
-        console.log(err ? 'Error' : 'Success')
-      );
+    .then(({ name, description }) => {
+      download('drich14/web-starter', name, err => {
+        console.log(err ? 'Error' : 'Success');
+
+        const options = {
+          files: [
+            `${name}/**/*`,
+            `${name}/.env*`
+          ],
+          from: [NAME, DESCRIPTION],
+          to: [name, description]
+        };
+
+        replace(options)
+          .then(changes => {
+            console.log('Modified files:', changes.join(', '));
+          })
+          .catch(error => {
+            console.error('Error occurred:', error);
+          });
+      });
     });
 };
